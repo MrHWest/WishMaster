72170a76fdfe1bfc0df82ee59d6ccc6120409c27
Mikael H <mikael.holsve@gmail.com>
2023-04-12 09:36:39 +0200

    first commit

---
+++ src/main/java/com/sabaton/wishmaster/WishMasterApplication.java
@@ -0,0 +1,13 @@
+package com.sabaton.wishmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishMasterApplication.class, args);
+    }
+
+}
