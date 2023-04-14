687fe0dace89c0d5302f36aefaf2b3313a530f3d
Mikael H <mikael.holsve@gmail.com>
2023-04-14 15:38:52 +0200

    made slight change to database creation query

--- src/main/resources/mySQL/create-db.sql
+++ src/main/resources/mySQL/create-db.sql
@@ -1,10 +1,10 @@
 DROP DATABASE IF EXISTS wishmaster;
 CREATE SCHEMA wishmaster;
-DROP TABLE IF EXISTS wishlist;
+DROP TABLE IF EXISTS wishmaster.wishlist;
 CREATE TABLE wishmaster.wishlist (
 	`id` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `password` VARCHAR(16) NOT NULL);
-DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS wishmaster.item;
 CREATE TABLE wishmaster.item (
 	`id` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `title` VARCHAR(40) NOT NULL,
