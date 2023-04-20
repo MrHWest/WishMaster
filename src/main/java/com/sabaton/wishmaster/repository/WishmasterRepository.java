9b67382ed077fe26dde404ffbd68e38944250836
Mikael H <mikael.holsve@gmail.com>
2023-04-20 15:29:31 +0200

    db changes

--- src/main/java/com/sabaton/wishmaster/repository/WishmasterRepository.java
+++ src/main/java/com/sabaton/wishmaster/repository/WishmasterRepository.java
@@ -40,7 +40,7 @@ public class WishmasterRepository {
                 int id = result.getInt("id");
                 String title = result.getString("title");
                 String link = result.getString("link");
-                int wishlistID = result.getInt("wishlistID");
+                int wishlistID = result.getInt("wishlist_id");
                 System.out.println(title + " " + link);
                 items.add(new Item(id, title, link, wishlistID));
             }
@@ -61,7 +61,7 @@ public class WishmasterRepository {
 
     public void addItem(String title, String link, int wishlistID){
 
-        String ADDITEM_QUERY = "INSERT INTO wishmaster.item(title, link, wishlistID) VALUES (?, ?, ?) ";
+        String ADDITEM_QUERY = "INSERT INTO wishmaster.item(title, link, wishlist_id) VALUES (?, ?, ?) ";
         ConnectionManager connectionManager = new ConnectionManager();
         try{
             Connection connection = connectionManager.getConnection(DB_URL, UID, PWD);
@@ -121,7 +121,7 @@ public class WishmasterRepository {
 
     public ArrayList<Item> getItemsFromId(int wishlistId) {
 
-        String SELECT_QUERY = "SELECT * FROM wishmaster.item WHERE wishlistID=?";
+        String SELECT_QUERY = "SELECT * FROM wishmaster.item WHERE wishlist_id=?";
         ConnectionManager connectionManager = new ConnectionManager();
         try {
             Connection connection = connectionManager.getConnection(DB_URL, UID, PWD);
@@ -135,7 +135,7 @@ public class WishmasterRepository {
                 int id = result.getInt("id");
                 String title = result.getString("title");
                 String link = result.getString("link");
-                int wishlistID = result.getInt("wishlistID");
+                int wishlistID = result.getInt("wishlist_id");
                 System.out.println(title + " " + link);
                 items.add(new Item(id, title, link, wishlistID));
             }
